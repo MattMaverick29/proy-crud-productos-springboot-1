0e930b71bced4ec4b542d83ae53a131c09f52d24
matia <matiasgutierrez23@gmail.com>
2020-12-10 19:04:18 -0300

    Se sube arquitectura base del proyecto

---
+++ src/main/java/com/matt/springboot/app/ProyCrudProductosSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.matt.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyCrudProductosSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyCrudProductosSpringboot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/matt/springboot/app/ProyCrudProductosSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyCrudProductosSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
